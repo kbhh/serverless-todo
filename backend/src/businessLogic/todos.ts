95eda636b11563c926c658540290e17e423d8445
kbhh <kbromhailay0@gmail.com>
2022-11-03 23:12:54 +0200

    folder updated

--- backend/src/businessLogic/todos.ts
+++ backend/src/businessLogic/todos.ts
@@ -1,4 +1,4 @@
-import { TodosAccess } from '../helpers/todosAcess'
+import { TodosAccess } from '../data/todosAcess'
 import { AttachmentUtils } from '../helpers/attachmentUtils';
 import { TodoItem } from '../models/TodoItem'
 import { CreateTodoRequest } from '../requests/CreateTodoRequest'
